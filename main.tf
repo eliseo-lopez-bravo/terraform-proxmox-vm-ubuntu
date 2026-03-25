e5a0d574b81aff85c05827b1918c0013036f9677
eliseo-lopez-bravo <eliseo.lopez.007@gmail.com>
2026-03-25 11:35:33 -0700

    minor tweak

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.14.0"
-
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "~> 0.99.0"
-    }
-  }
-}
-
 #####################
 # Ubuntu 24 VM
 #####################
--- providers.tf
+++ providers.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.14.0"
+
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.99.0"
+    }
+  }
+}
+
 provider "proxmox" {
   endpoint   = var.proxmox_api_url
   api_token  = var.proxmox_api_token
